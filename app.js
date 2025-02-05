cd2fad7961eaa14c3de1681d072f2d5f0cb59f98
Adrian Oliveros <adrianrobbieoliveros@gmail.com>
2025-02-05 11:59:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+// Selecciona el contenedor
+let contenedor = document.getElementById("info-perfil");
 
+// Crea elementos
+let nombre = document.createElement("h1");
+nombre.textContent = "Fanático del Fútbol";
+
+let descripcion = document.createElement("p");
+descripcion.textContent = "Amante del deporte rey. ¡Sígueme para más contenido!";
+
+// Inserta los elementos en el contenedor
+contenedor.appendChild(nombre);
+contenedor.appendChild(descripcion);
